0cbf138949b8e03a6485d5b13f681db623f2389d
JUNIOR_OKOSTA <82908127+JUNIOROKOSTA@users.noreply.github.com>
2022-12-08 15:36:44 -0300

    add js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Alá Mundo!!')
+var fieldName = document.querySelector('#exampleInputName')
+var fieldGender = document.querySelectorAll("#form-user-create [name=gender]:checked")
+var fieldBirth = document.querySelector('#exampleInputBirth')
+var fieldCountry = document.querySelector('#exampleInputCountry')
+var fieldEmail = document.querySelector('#exampleInputEmail1')
+var fieldPassword = document.querySelector('#exampleInputPassword1')
+var fieldFile = document.querySelector('#exampleInputFile')
+var fieldCheckboxAdmin = document.querySelector('#form-user-create [name=admin]')
+
