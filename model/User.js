8adf44fda23014e2ff5d4e9555a6175c2c7fe14d
JUNIOR_OKOSTA <82908127+JUNIOROKOSTA@users.noreply.github.com>
2022-12-09 10:29:12 -0300

    init MVC object
    Implementando modelo MVC no projeto.

---
+++ model/User.js
@@ -0,0 +1,12 @@
+class User {
+    constructor(name, gender, birth, country, email, password, admin, photo){
+        this.name = name;
+        this.gender = gender;
+        this.birth = birth;
+        this.country = country;
+        this.email = email;
+        this.password = password;
+        this.admin = admin;
+        this.photo = photo;
+    }
+}
